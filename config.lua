c9da58079d5406dc6ae2b4adbba27edd1f0d8119
iWanheda <lexihvh@gmail.com>
2021-08-30 22:55:58 +0100

    Fixed a weird VS Code glitch

--- config.lua
+++ config.lua
@@ -1,32 +1,32 @@
 Config =
 {
-	ServerName = 'ZimaN',
-	Debug = true,
-	AutoSaveTime = 2, -- In Minutes
-	Queue = true, -- Use a custom queue system
+  ServerName = 'ZimaN',
+  Debug = true,
+  AutoSaveTime = 2, -- In Minutes
+  Queue = true, -- Use a custom queue system
 
   SpawnLocation = vector3(-428.6901, 1111.886, 327.6732),
 
-	SpawnPeds = false, -- Should we spawn default GTA MP peds on the streets?
+  SpawnPeds = false, -- Should we spawn default GTA MP peds on the streets?
 
-	-- Player will be given these items upon first join
-	DefaultInventory =
-	{
-		idcard = 1,
-		phone = 1,
-		cookie = 5,
-		water = 5
-	},
-	
-	MoneyTypes = 
-	{
-		cash = 1100,
-		bank = 35000,
-		crypto = 0
-	},
+  -- Player will be given these items upon first join
+  DefaultInventory =
+  {
+    idcard = 1,
+    phone = 1,
+    cookie = 5,
+    water = 5
+  },
+    
+  MoneyTypes = 
+  {
+    cash = 1100,
+    bank = 35000,
+    crypto = 0
+  },
 
-	Identifier = 'license',
-	BanType = 'TOKEN',
+  Identifier = 'license',
+  BanType = 'TOKEN',
 
   -- Static Data
 
